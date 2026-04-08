c64c7111673a8c1fad306db69d1a0449bcbe0e9e
flix2026 <flix2026@users.noreply.github.com>
2026-04-08 01:22:14 +0000

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -4,19 +4,10 @@ GITHUB_USERNAME="flix2026"
 GITHUB_TOKEN="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"
 REPO="Flix-Vision"
 
-git config --global user.email "flix2026@users.noreply.github.com"
-git config --global user.name "flix2026"
-
-REMOTE="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${REPO}.git"
-
-if [ ! -d ".git" ]; then
-    git init
-    git remote add origin "$REMOTE"
-else
-    git remote set-url origin "$REMOTE"
-fi
+git config user.email "flix2026@users.noreply.github.com"
+git config user.name "flix2026"
+git remote set-url origin "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_USERNAME}/${REPO}.git"
 
 git add -A
 git commit -m "Update $(date '+%Y-%m-%d %H:%M:%S')"
-git branch -M main
-git push -u origin main --force
+git push origin main --force
